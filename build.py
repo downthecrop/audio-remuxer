794e2165959bf2fbb68645bfb1024120a3ca3ba6
crop <bryce@mathx.ca>
2020-09-06 00:02:06 -0700

    Initial commit

---
+++ build.py
@@ -0,0 +1,36 @@
+#Requires cx_freeze
+#build command: python build.py build
+#
+import sys
+from cx_Freeze import setup, Executable
+
+build_exe_options = {
+    "packages": [
+        "os"
+    ], 
+    "excludes": [
+        "tkinter", 
+        "PyQt4.QtSql", 
+        "sqlite3", 
+        "scipy.lib.lapack.flapack",
+        "PyQt4.QtNetwork",
+        "PyQt4.QtScript",
+        "numpy.core._dotblas", 
+        "PyQt5"
+    ],
+    "optimize": 2
+    }
+        
+target = Executable(
+    script="ffmpeg-remux-audio.py",
+    icon="lib\icon.ico"
+    )
+
+setup(
+    name="Audio-Remuxer",
+    version="1.0",
+    description="FFmpeg audio track remuxer",
+    author="downthecrop",
+    options = {"build_exe": build_exe_options},
+    executables=[target]
+    )
---
+++ ffmpeg-remux-audio.py
@@ -0,0 +1,60 @@
+import os
+import configparser
+
+def get_cfg(key):
+    config = configparser.ConfigParser()
+    config.read('settings.cfg')
+    val = config.get('SETTINGS',key,raw=True)
+    return val.strip('"')
+
+def get_paths():
+    files = [f for f in os.listdir(inDir) if os.path.isfile(os.path.join(inDir, f))]
+    paths = [os.path.join(inDir, basename) for basename in files]
+    paths = [x for x in paths if ".mp4" in x]
+    return paths
+    
+def exec_remux(inFile, outFile):
+    #https://trac.ffmpeg.org/wiki/Map
+    cmd = '{} -i "{}" -map 0:0 -map 0:{} -c:a copy\
+           -c:v copy -preset {} -f mp4 "{}"'
+
+    cmd = cmd.format(ffmpeg,inFile,audioChannel,encodePreset,outFile)
+
+    os.system("{}".format(cmd))
+
+    if deletemkv == 'True':
+        mkv = inFile.replace(".mp4",".mkv")
+        try:
+            os.remove(mkv)
+        except:
+            print("\033[95mUnable to delete MKV Source: "+mkv+"\033[0m")
+
+#Load settings from settings.cfg
+inDir        = get_cfg('inDir')
+outDir       = get_cfg('outDir')
+outPrefix    = get_cfg('outPrefix')
+ffmpeg       = get_cfg('ffmpeg')
+encodePreset = get_cfg('encodePreset')
+audioChannel = get_cfg('audioChannel')
+deletemkv    = get_cfg('deletemkv')
+batchMode    = get_cfg('batchMode')
+
+#Default FFmpeg to included binary
+if not ffmpeg:
+    path = os.path.dirname(os.path.realpath(__file__))
+    ffmpeg = path+"\\lib\\ffmpeg.exe"
+
+#Remux
+if batchMode == 'True':
+    for f in get_paths():
+        inFile = f
+        outFile = outDir+outPrefix+os.path.basename(inFile)
+        exec_remux(inFile,outFile)
+else:
+    try:
+        inFile  = max(get_paths(), key=os.path.getctime) #Latest mp4
+        outFile = outDir+outPrefix+os.path.basename(inFile)
+        exec_remux(inFile,outFile)
+    except:
+        print("Please configure in and out directories in settings.cfg")
+        input("Press ENTER to close this window.")
